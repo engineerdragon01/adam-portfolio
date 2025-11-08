e48d7b840e9de4a3cceb16aa0479b76a0fe810cd
engineerdragon01 <dragonboy174@gmail.com>
2025-11-07 18:37:06 -0800

    Update favicon and site header

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,4 +1,4 @@
-import TypingAnimation from './TypingAnimation';
+import TypingAnimation from './components/TypingAnimation';
 import { useState, useEffect } from 'react';
 
 import './css/styles.css';
